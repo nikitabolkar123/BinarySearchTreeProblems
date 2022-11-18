23c0871b0b9bcc7c1cfa03a7867c05c98fc01c03
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-18 03:22:51 -0800

    Added Solution

---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    public class Node<T>
+    {
+        public T data;
+        public Node<T> leftNode;
+        public Node<T> rightNode;
+        public Node(T data)
+        {
+            this.data = data;
+            this.leftNode = null;
+            this.rightNode = null;
+        }
+    }
+}
---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>();
+            binarySearchTree.InsertData(56);
+            binarySearchTree.InsertData(30);
+            binarySearchTree.InsertData(70);
+            binarySearchTree.Display(binarySearchTree.GetRoot());
+        }
+    }
+}
+
